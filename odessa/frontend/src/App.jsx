26b41a309adeee54448fdbbd2a77a70adfdd5faf
AlejandroAlv12 <00211623@uca.edu.sv>
2025-10-29 14:53:12 -0600

    feat: corregir app.jsx

--- odessa/frontend/src/App.jsx
+++ odessa/frontend/src/App.jsx
@@ -1,45 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import React from 'react'
-=======
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
->>>>>>> parent of add5562 (feat: agregar paginas FE)
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-<<<<<<< HEAD
-=======
+import React from 'react';
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import './App.css';
 
 function App() {
   return (
@@ -51,8 +13,6 @@ function App() {
 }
 
 export default App;
->>>>>>> eda8afd64bb009ae7c33eed6d7bc5721e509975d
-=======
 
-export default App
->>>>>>> parent of add5562 (feat: agregar paginas FE)
+
+
